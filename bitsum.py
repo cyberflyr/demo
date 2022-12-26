5e432a66167d8fb1f0fcef92d37b532407b52c23
Zhuangzhi Guo <zhuangzhi-infstones@infstones.com>
2022-12-27 00:05:06 +0800

    update

--- bitsum.py
+++
@@ -1,8 +0,0 @@
-
-def bit_sum(a: list, b: list):
-    i = 0
-    overflow = 0
-    res = []
-    while i < len(a) and i < len(b):
-        res.append(a[i]^b[i])
-        overflow =
--- test_encrypt.py
+++
@@ -1,4 +0,0 @@
-if __name__ == "__main__":
-    base = 12
-    res = pow(base, 5) % 35
-    print(res)
